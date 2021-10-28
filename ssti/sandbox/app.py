413e2a058304b7c899b73617a87f55df4490a3fd
Aplet123 <aplet@aplet.me>
2021-10-28 15:45:03 -0400

    ro

--- ssti/sandbox/app.py
+++ ssti/sandbox/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, render_template_string, request
+from jinja2 import TemplateError
 
 app = Flask(__name__, static_url_path="/static", static_folder="./static")
 flag = "flag{placeholder_flag}"
 
 @app.route("/render", methods = ["GET"])
 def render_route():
-    return render_template_string(request.args.get("t", ""))
+    try:
+        return render_template_string(request.args.get("t", ""))
+    except TemplateError as e:
+        return ("got error: " + str(e), 500)
 
 @app.route("/")
 def index():
